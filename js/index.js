b0bf7e577ac98bc7943ae85416ce90e526d2879d
sashabull66 <sashabull9042@gmail.com>
2021-08-19 17:14:08 +0300

    set position for 3 level

--- js/index.js
+++ js/index.js
@@ -45,7 +45,7 @@ export const initialState = new useState({
                 playerName: null,
             }
         },
-        currentLevel: 10,
+        currentLevel: 3,
         currentItems: null, // то что в меню
         images: {
             gameMenuBG: '/../images/game/gameMenu/gameMenu.png'
